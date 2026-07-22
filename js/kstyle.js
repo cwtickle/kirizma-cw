a14813b8c8730689c6c5e0d6022f4c5c38177133
tickle <qu7.52m@gmail.com>
2026-07-22 21:39:19 +0900

    [ver3.0.0] キリズマ以外の譜面との共存に対応

--- js/kstyle.js
+++ js/kstyle.js
@@ -4,12 +4,12 @@
  * [for Kirizma]
  * 
  * Created: 2022/09/17
- * Revised: 2026/03/15
- * Source Version: Ver 2.3.2
+ * Revised: 2026/07/22
+ * Source Version: Ver 3.0.0
  * 
  * https://github.com/cwtickle/kirizma-cw
  */
-const g_kirizmaVersion = `Ver 2.3.2`;
+const g_kirizmaVersion = `Ver 3.0.0`;
 
 /**
  * タイトル画面前の割込み処理（初回譜面読込後）
