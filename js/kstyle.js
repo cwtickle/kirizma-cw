57b2b37260cf2168ed40073b1aed39ce98f278c8
tickle <qu7.52m@gmail.com>
2026-08-11 21:06:23 +0900

    [ver3.0.2] キリズマでのオプション無効化を解除

--- js/kstyle.js
+++ js/kstyle.js
@@ -5,11 +5,11 @@
  * 
  * Created: 2022/09/17
  * Revised: 2026/08/11
- * Source Version: Ver 3.0.1
+ * Source Version: Ver 3.0.2
  * 
  * https://github.com/cwtickle/kirizma-cw
  */
-const g_kirizmaVersion = `Ver 3.0.1`;
+const g_kirizmaVersion = `Ver 3.0.2`;
 
 /**
  * タイトル画面前の割込み処理（初回譜面読込後）
